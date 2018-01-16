1deedcd8a1d03867b51d52e23a01ef96f4507eea
Newton Bii <biinewton382@gmail.com>
2018-01-16 10:34:57 +0300

    Add tests.

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -6,4 +6,21 @@ class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
-    
+
+    def __str__(self):
+        return self.first_name
+    class Meta:
+        ordering = ['first_name']
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    published_date = models.DateTimeField(auto_now_add=True)
--- tribune/news/tests.py
+++ tribune/news/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from .models import Editor, Article, tags
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.newton = Editor(first_name='newton', last_name='bii', email='biinewtondev@gmail.com')
+
+    #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.newton, Editor))
+
+    #test save method
+    def test_save_method(self):
+        self.newton.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors)>0)
