6be67055f56c8530fdc09ccb7716a5f5cfa667a3
Newton Bii <biinewton382@gmail.com>
2018-01-17 12:33:40 +0300

    add media route to display images on development server .

--- tribune/tribune/settings.py
+++ tribune/tribune/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -116,6 +117,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Static files (CSS, JavaScript, Images)
